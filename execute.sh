0922eaa0dee6379da8841a2e122d75e0cab70d6b
cedwic <cedricherve@gmail.com>
2018-09-18 14:46:38 +0200

    debug ci

--- execute.sh
+++ execute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # to run script sudo sh execute.sh
 # test lambda
-newman run GGWG.postman_collection -e GGWG-PROD.postman_environment --color on cli,html --reporter-html-export ggwg-test-resut.html --reporter-junit-export /tmp/test-results
+newman run GGWG.postman_collection -e GGWG-PROD.postman_environment --reporter-html-export ggwg-test-resut.html --reporter-junit-export /tmp/test-results
 
