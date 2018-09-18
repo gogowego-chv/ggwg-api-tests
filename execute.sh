30caf45f2834a73075cdd260f6e7587506b18efc
cedwic <cedricherve@gmail.com>
2018-09-18 12:25:42 +0200

    debug circle ci

--- execute.sh
+++ execute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # to run script sudo sh execute.sh
 # test lambda
-newman run GGWG.postman_collection -e GGWG-PROD.postman_environment --color -r cli,html --reporter-html-export ggwg-test.html
+newman run GGWG.postman_collection -e GGWG-PROD.postman_environment --color cli,html --reporter-html-export ggwg-test.html
 
