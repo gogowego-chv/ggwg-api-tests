dc87df94b1297ded88ea24482ec72b923007e6fb
cedwic <cedricherve@gmail.com>
2018-09-19 16:00:26 +0200

    Update config.yml

--- execute.sh
+++ execute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # to run script sudo sh execute.sh
 # test lambda
-newman run GGWG.postman_collection -e GGWG-PROD.postman_environment -r cli,html,junit --bail
+newman run GGWG.postman_collection --bail -e GGWG-PROD.postman_environment -r cli,html,junit
 junit-merge -r ./newman -o /newman/results
 
